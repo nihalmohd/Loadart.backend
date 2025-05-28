a020e2bc56ac41def5d978f5652432a85d8d2ac2
Ubuntu <ubuntu@ip-172-31-5-13.ap-south-1.compute.internal>
2025-05-28 10:14:36 +0000

    AddLoad api changed

--- controller/UsersController/AddLoad.js
+++ controller/UsersController/AddLoad.js
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-import pool from "../../Model/Config.js";
-
-export const addLoad = async (req, res) => {
-    const {
-        pickupLoc,
-        deliveryLoc, 
-        pickupDate,
-        material_id,
-        capacity_id,
-        truck_type_id,
-        comment,
-        user_id,
-        no_of_trucks,
-    } = req.body;
-
-    if (
-        !pickupLoc ||
-        !deliveryLoc ||
-        !pickupDate || !capacity_id || !user_id ||
-        !no_of_trucks
-    ) {
-        return res.status(400).json({ message: "All fields except 'comment' are mandatory." });
-    }
-
-   
-    const insertLoadQuery = `
-        INSERT INTO loadart.loads 
-        ("pickupLoc", "deliveryLoc", "pickupDate", "material_id", "capacity_id",  "truck_type_id", "comment", "user_id", "no_of_trucks")
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-        RETURNING *;
-    `;
-
-    try {
-        const insertValues = [
-            pickupLoc,
-            deliveryLoc,
-            pickupDate,
-            material_id,
-            capacity_id,
-            truck_type_id,
-            comment || null, 
-            user_id,
-            no_of_trucks,
-        ];
-
-        const result = await pool.query(insertLoadQuery, insertValues);
-
-        if(result){
-            res.status(201).json({
-                message: "Load added successfully",
-                data: result.rows[0],
-            });
-        }else{
-            res.status(422).json({message:"Unprocessable Entity please check values in table and your entry"})
-        }
-
-    } catch (error) {
-        console.error("Error adding load:", error.message);
-        res.status(500).json({ message: "Internal server error" });
-    }
-};
-=======
 import pool from "../../Model/Config.js";
 
 export const addLoad = async (req, res) => {
@@ -124,4 +61,3 @@ export const addLoad = async (req, res) => {
     }
 };
 
->>>>>>> 663ca9c01ea460882333fff5de8798075e7db799
